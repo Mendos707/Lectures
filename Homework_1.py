f30f91dd4d05a5048e4c4e14279c8005b1d0171b
Mendos707 <105000282+Mendos707@users.noreply.github.com>
2022-05-09 01:33:57 +0300

    Update Homework_1.py

--- Homework_1.py
+++ Homework_1.py
@@ -1,20 +1,4 @@
-'''
-Написать программу которая:
-	1. Высчитывает возраст из заданых данных (current_year - нынешний год, year_of_birth - год рождения)
-	2. Найти недостающую часть кода (code_2)
-		a. найдите остаток от x деленого на y
-		b. полученый результ умножте на 13
-		c. извлеките квадратный корень из полученного результата
-		d. возьмите целую часть от результа
-	3. Соединить код в отдельную переменную
-	    пример:
-	        475-12-967
-	4. Вывести строку:
-		пример:
-			Hello Mary Gold. You are 26 years old. Your secret code is 475-12-967.
-'''
 
-# years
 current_year = 2022
 year_of_birth = 1988
 code_1 = '354'
@@ -29,25 +13,19 @@ y = 132
 
 def work1():
     age = current_year - year_of_birth
-    # print(age)
     return age
 
 
 def work3():
     code = code_1 + '-' + str(work2()) + '-' + str(code_3)
-    # print(code)
     return code
 
 
 def work2():
     remainder = x % y
-    # print(remainder)
     remainder = remainder * 13
-    # print(remainder)
     remainder = remainder ** 0.5
-    # print(remainder)
     remainder = round(remainder)
-    # print(remainder)
     return remainder
 
 
